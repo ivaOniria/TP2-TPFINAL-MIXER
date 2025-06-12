0cf76be1651b1e5f58a11f02d2d71774e35e18a3
Ivan Liscovsky <liscovsky.ivan@hotmail.com>
2025-06-11 22:27:38 -0300

    userSchema

--- model/DAO/models/user.js
+++ model/DAO/models/user.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const productoSchema = mongoose.Schema({ // MODIFICAR PARA USER
+const userSchema = mongoose.Schema({ // MODIFICAR PARA USER
     nombre: String,
     precio: Number,
     stock: Number
 },{versionKey: false})
 
-export const UserModel = mongoose.model('users', productoSchema)
+export const UserModel = mongoose.model('users', userSchema)
