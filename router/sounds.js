e51fd37a93b1d5a2fb3efa4e08226d3e1511f458
Ivan <isch.80@gmail.com>
2025-06-29 16:18:02 -0300

    feat: Implementación de renderizado FFmpeg y lógica de endpoint
    Este commit introduce la funcionalidad de renderizado a través de FFmpeg, incluyendo la lógica del endpoint y la comunicación con el servicio.

--- router/sounds.js
+++ router/sounds.js
@@ -13,8 +13,10 @@ class Router {
         const router = express.Router()
 
         router.get('/loadSounds/:id', this.#controlador.cargarSonidos)
+        router.get('/download/:filename', this.#controlador.descargarArchivo)
         router.get('/:id?', this.#controlador.obtenerSonidos)
         router.post('/', this.#controlador.guardarSonido)
+        router.post('/render', this.#controlador.renderizarSonidos)   
         router.put('/:id', this.#controlador.actualizarSonido)
         router.delete('/:id', this.#controlador.borrarSonido)
 
